45970bc7ae03d8fc41b7bade605f62fc74325f6f
mitchellcooperw <mitchell.cooper.w@gmail.com>
2024-06-29 12:45:33 -0400

    readme

---
+++ README.md
@@ -0,0 +1,6 @@
+How to deploy to another environment
+    * https://flask.palletsprojects.com/en/3.0.x/tutorial/deploy/
+    * https://flask.palletsprojects.com/en/3.0.x/deploying/
+
+Find other Flask extensions
+* https://pypi.org/search/?c=Framework+%3A%3A+Flask
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,6 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
+        # python -c 'import secrets; print(secrets.token_hex())' to create a random key
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
